0a63ca0bb135d7037ea166ee6d840d9103efccc9
Cal <cwolfson58@gmail.com>
2024-12-06 21:16:25 +1100

    fix events merge

--- src/main/java/at/hannibal2/skyhanni/features/garden/GardenOptimalAngles.kt
+++ src/main/java/at/hannibal2/skyhanni/features/garden/GardenOptimalAngles.kt
@@ -1,5 +1,6 @@
 package at.hannibal2.skyhanni.features.garden
 
+import at.hannibal2.skyhanni.api.event.HandleEvent
 import at.hannibal2.skyhanni.events.GardenToolChangeEvent
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
 import at.hannibal2.skyhanni.utils.LorenzColor
@@ -70,7 +71,7 @@ object GardenOptimalAngles {
         )
     }
 
-    @SubscribeEvent
+    @HandleEvent
     fun onGardenToolChange(event: GardenToolChangeEvent) {
         cropInHand = event.crop
         event.crop?.let { lastCrop = it }
