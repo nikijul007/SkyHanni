fed8b8dd8438dcc0c36b0d849ca2e1af674f3054
calwolfson <cwolfson58@gmail.com>
2025-01-09 16:52:36 +0100

    make kotlin, dictionary

--- src/main/java/at/hannibal2/skyhanni/config/features/garden/optimalAngles/OptimalAnglesConfig.kt
+++ src/main/java/at/hannibal2/skyhanni/config/features/garden/optimalAngles/OptimalAnglesConfig.kt
@@ -1,32 +1,34 @@
-package at.hannibal2.skyhanni.config.features.garden.optimalAngles;
+package at.hannibal2.skyhanni.config.features.garden.optimalAngles
 
-import at.hannibal2.skyhanni.config.FeatureToggle;
-import at.hannibal2.skyhanni.config.core.config.Position;
-import com.google.gson.annotations.Expose;
-import io.github.notenoughupdates.moulconfig.annotations.Accordion;
-import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorBoolean;
-import io.github.notenoughupdates.moulconfig.annotations.ConfigLink;
-import io.github.notenoughupdates.moulconfig.annotations.ConfigOption;
+import at.hannibal2.skyhanni.config.FeatureToggle
+import at.hannibal2.skyhanni.config.core.config.Position
+import com.google.gson.annotations.Expose
+import io.github.notenoughupdates.moulconfig.annotations.Accordion
+import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorBoolean
+import io.github.notenoughupdates.moulconfig.annotations.ConfigLink
+import io.github.notenoughupdates.moulconfig.annotations.ConfigOption
 
-public class OptimalAnglesConfig {
+class OptimalAnglesConfig {
     @Expose
-    @ConfigOption(name = "Squeaky Mousemat", desc = "Set the optimal angles (pitch & yaw) in the Squeaky Mousemat overlay by clicking on the presets.")
+    @ConfigOption(
+        name = "Squeaky Mousemat",
+        desc = "Set the optimal angles (pitch & yaw) in the Squeaky Mousemat overlay by clicking on the presets."
+    )
     @ConfigEditorBoolean
     @FeatureToggle
-    public boolean signEnabled = true;
+    var signEnabled: Boolean = true
 
     @Expose
-    @ConfigOption(name = "Compact GUI", desc = "Compact the Squeaky Mousemat GUI only showing crop icons")
+    @ConfigOption(name = "Compact GUI", desc = "Compact the Squeaky Mousemat GUI only showing crop icons.")
     @ConfigEditorBoolean
-    public boolean compactMousematGui = false;
+    var compactMousematGui: Boolean = false
 
     @Expose
-    @ConfigLink(owner = OptimalAnglesConfig.class, field = "signEnabled")
-    public Position signPosition = new Position(20, -195, false, true);
+    @ConfigLink(owner = OptimalAnglesConfig::class, field = "signEnabled")
+    var signPosition: Position = Position(20, -195, false, true)
 
     @Expose
     @ConfigOption(name = "Custom Speed", desc = "Change the exact speed for every single crop.")
     @Accordion
-    public CustomAnglesConfig customAngles = new CustomAnglesConfig();
-
+    var customAngles: CustomAnglesConfig = CustomAnglesConfig()
 }
